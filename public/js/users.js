3ea0f36f367f268dbdc2420badb524f9720e0593
Eduardo Pilati <pilatieduardo@gmail.com>
2021-01-16 14:09:52 -0300

    Show loader on AJAX requests

--- public/js/users.js
+++ public/js/users.js
@@ -11,6 +11,8 @@ function modalListeners(){
 
 function sendEmail(event){
     event.preventDefault()
+    loader.show();
+
     $.getJSON({
         url: urlsendemail
     }).done(function(data){
@@ -19,40 +21,54 @@ function sendEmail(event){
         setTimeout(() => {
             $('#message').slideUp();
         }, 5000);
+    }).always(function(){
+        loader.hide();
     })
 }
 
 function userGroups(event){
     event.preventDefault()
+    loader.show();
+
     $.get({
         url: urlusergroups
     }).done(function(html){
         $('#users-modal').html(html)
         $('#users-modal .modal').modal('show')
         modalListeners()
+    }).always(function(){
+        loader.hide();
     })
 }
 
 function linkUserGroup(event){
     event.preventDefault();
     var groupId = $(this).data('group')
+    loader.show();
+
     $.post({
         url: urllinkusergroup,
         data: { 'group_id' : groupId },
         headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
     }).done(function(){
         window.location.reload();
+    }).always(function(){
+        loader.hide();
     })
 }
 
 function unlinkUserGroup(event){
     event.preventDefault();
     var groupId = $(this).data('group')
+    loader.show();
+
     $.post({
         url: urlunlinkusergroup,
         data: { 'group_id' : groupId },
         headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
     }).done(function(){
         window.location.reload();
+    }).always(function(){
+        loader.hide();
     })
 }
